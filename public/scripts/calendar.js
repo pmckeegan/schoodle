fcd10acbd1c8922755ba85a1b7aa912c539e7117
pmckeegan <pmckeegan@gmail.com>
2019-03-30 00:42:20 +0000

    calendar.js

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,7 +1,9 @@
 
-
 $(document).ready(function() { 
 
+  // pg.insert(knex.select().from('options_date').insert({date_option1: dateChoices.flatpickr1}))
+  // pg.insert(knex.select().from('options_date').insert({date_option2: dateChoices.flatpickr2}))
+  // pg.insert(knex.select().from('options_date').insert({date_option3: dateChoices.flatpickr3}))
   
   var dateChoices={};
 
@@ -12,15 +14,13 @@ $(document).ready(function() {
       var id = ($(this.element).attr('id'));
       dateChoices[id]=(selectedDates[0]);
       Object.values(dateChoices);
-
       
+      console.log(dateChoices.flatpickr1);
+      console.log(dateChoices.flatpickr2);
+      console.log(dateChoices.flatpickr3);
+
       $("#createEvent").click(function() {
-        $.post("/:event_id/guest_confirmation", {dateChoices}, 
-        function(data, status)
-        { 
-          
-        }
-        ajax.post
+
       });
     }   
   });
